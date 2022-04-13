ea530563a5676a8aa00f9ac5470299ac3d26b013
gisele <joyk9251@gmail.com>
2022-04-13 16:58:06 +0900

    set drag and drop

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -1,5 +1,35 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
 function App() {
-  return null;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId={"one"}>
+          {(magic) => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
+              <Draggable draggableId={"1"} index={0}>
+                {(provide) => (
+                  <li ref={provide.innerRef} {...provide.draggableProps}>
+                    <span {...provide.dragHandleProps}>🔥</span>
+                    11
+                  </li>
+                )}
+              </Draggable>
+              <Draggable draggableId={"2"} index={1}>
+                {(provide) => (
+                  <li ref={provide.innerRef} {...provide.draggableProps}>
+                    <span {...provide.dragHandleProps}>🔥</span>
+                    22
+                  </li>
+                )}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 }
 
 export default App;
